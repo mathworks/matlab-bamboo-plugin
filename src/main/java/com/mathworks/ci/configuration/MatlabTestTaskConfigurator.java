2e5235cd80244648218670ccaa5ca4c00273661f
ppandian28 <54954089+ppandian28@users.noreply.github.com>
2021-12-16 15:14:33 -0500

    removing unused logger dependency.

--- src/main/java/com/mathworks/ci/configuration/MatlabTestTaskConfigurator.java
+++ src/main/java/com/mathworks/ci/configuration/MatlabTestTaskConfigurator.java
@@ -15,8 +15,6 @@ import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.struts.TextProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.mathworks.ci.helper.MatlabBuilderConstants;
 import javax.inject.Inject;
 import java.util.ArrayList;
